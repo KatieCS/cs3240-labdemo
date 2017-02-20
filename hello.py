0fa9b5866c0b17a9625f8e8101ea56a3f63d9a6e
Kaytay <kmv3qt@virginia.edu>
2017-02-20 17:45:08 -0500

    Added accusation function to helper, modified hello, changed output.txt.

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
 from helper import greeting
+from helper import accusation
 greeting("hello")
-
+greeting("you are all my precious friends")
+accusation("you were all my precious friends")
--- helper.py
+++ helper.py
@@ -1,3 +1,6 @@
 def greeting(msg):
     print(msg)
     return
+
+def accusation(msg):
+    print('\"', msg, ',\" they say accusingly.')
